2b119d627db03549c5a5a20e099a5abb33126071
Apu <abdulazizapu@gmail.com>
2022-04-06 18:13:57 +0600

    setting up config folder and connection to mongo database

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
-const app = express()
+connectDB();
+
+const app = express();
+
+
 
 app.get('/', (req, res) => res.send('Hello World, Hi Hi'))
 
